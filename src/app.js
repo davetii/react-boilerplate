d082d72b1655db7bbe4e708540081f1d259e2746
Dave Turner <davetii@gmail.com>
2018-10-14 22:27:21 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/style.scss'
+import 'normalize.css/normalize.css'
+
+ReactDOM.render(<p>Expensify</p>, document.getElementById('app'));
